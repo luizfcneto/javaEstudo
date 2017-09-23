8700877afb5955afad07b67bbb3b66cc30604b76
Luiz <luizfcneto123@gmail.com>
2017-09-23 15:59:37 -0300

    +Olá Mundo java iniciando

---
+++ Aula01/HelloWorld.java
@@ -0,0 +1,34 @@
+//Classe HelloWorld
+class HelloWorld {
+	
+	//Funçao main(principal) do Hello World
+	public static void main (String[] args) {
+
+		//Comando para printar na tela
+		System.out.println("Hello World");
+	}
+}
+
+
+/*
+Desmembrando o Código:
+
+1*[class] 2*[HelloWorld] {
+	
+	//Funçao main(principal) do Hello World
+	3*[public static] 4*[void] 5*[main] (6*[String[] args]) {7*[
+
+		//Comando para printar na tela
+		System.out.println("Hello World");
+	]}
+}
+
+1- Class = Declaração da Classe
+2- HelloWorld = Nome da Classe (primeira letra maiuscula) 
+3- Modificadores de Acesso do Método
+4- Tipo de Retorno (void = sem retorno)
+5- Nome do método (main -> letra minuscula)
+6- Parâmetros/Argumentos do método
+7- Bloco de instrução do método main
+
+*/
