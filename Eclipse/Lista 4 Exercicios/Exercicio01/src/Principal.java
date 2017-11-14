c56a8e626bfe85909fff429cf9c4782283bc6a08
Luiz Fernando (luizfcneto <luizfcneto123@gmail.com>
2017-11-14 00:00:14 -0200

    +Lista de Exercicios 4

---
+++ Eclipse/Lista 4 Exercicios/Exercicio01/src/Principal.java
@@ -0,0 +1,46 @@
+/*
+ * 		Exercicio 01:
+ * 	Autor: Luiz Fernando (luizfcneto)
+ * 	Email: luizfcneto123@gmail.com	
+ * 	Descrição: Passar elementos de um vetor B para A
+ * 	Entrada: NULL
+ * 	Saida: NULL
+ * 
+ */
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int[] numerosA = new int[5];
+		int[] numerosB = new int[5];
+		int i = 0;
+		
+		numerosA[0] = 0;
+		numerosA[1] = 1;
+		numerosA[2] = 2;
+		numerosA[3] = 3;
+		numerosA[4] = 4;
+		
+		/*
+		 * For Tradicional: 
+		for (; i < numerosA.length; i++ ) {
+			numerosB[i] = numerosA[i];
+		}
+		*/
+		
+		//enchanced for:
+		for ( int numeros : numerosA) {
+			numerosB[i] = numeros; 
+			i++;
+		}
+		
+		
+		for ( i = 0; i < numerosA.length; i++ ) {
+			System.out.println("Numeros A: "+ numerosA[i] + " Numeros B: "+ numerosB[i]);
+		}
+	
+		
+	}
+	
+}
---
+++ Eclipse/Lista 4 Exercicios/Exercicio04/src/Principal.java
@@ -0,0 +1,41 @@
+/*
+ * 		Exericio 04
+ * 	Autor: Luiz Fernando (luizfcneto)
+ * 	Email: luizfcneto123@gmail.com	
+ * 	Descrição: VetorA[15] -> double, Construir B 
+ *   mesmo que A, sendo cada elemento de B = sqrt(A)
+ * 	Entrada: NULL
+ *	Saida: NULL
+ * 
+ */
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double[] vetorA = new double[15];
+		double[] vetorB = new double[vetorA.length];
+		int i = 0;
+		
+		//inserindo no vetor A:
+		for (; i < vetorA.length; i++) {
+			vetorA[i] = i;
+		}
+		
+		
+		i = 0;
+		//transferindo dados de A para B:
+		for ( double elementos : vetorA) {
+			vetorB[i] = Math.sqrt(vetorA[i]);
+			i++;
+		}
+		
+		
+		//imprimindo vetores A e B:
+		for ( i = 0; i < vetorA.length; i++) {
+			System.out.println("VetorA: "+vetorA[i]+" VetorB : "+vetorB[i]);
+		}
+		
+		
+	}
+	
+}
