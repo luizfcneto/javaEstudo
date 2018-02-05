8c5ad2f9363d0cd6741047b79aedcc7ad0756909
Luiz <luizfcneto123@gmail.com>
2018-02-05 20:43:08 -0200

    +Alguns exericios lista7 Feitos

---
+++ Eclipse/Listas Loiane/Lista 7 Exercicios/Lampada.java
@@ -0,0 +1,13 @@
+public class Lampada{
+	
+	public boolean ligada = false;
+
+	public void ligaLampada(){
+		this.ligada  = true;
+	}
+
+	public void desligaLampada(){
+		this.ligada = false;
+	}
+
+}
