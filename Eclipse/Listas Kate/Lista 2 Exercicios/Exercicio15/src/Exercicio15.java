e8c191d9b2ed6da117ec2d0a9c984b129e194082
Luiz Fernando (luizfcneto <luizfcneto123@gmail.com>
2018-03-06 23:37:54 -0300

    +Feitos mais 4 exercicios da Lista 2

---
+++ Eclipse/Listas Kate/Lista 2 Exercicios/Exercicio15/src/Exercicio15.java
@@ -0,0 +1,33 @@
+/*
+ *		Exercicio 15
+ *	Autor: Luiz Fernando (Luizfcneto) 
+ * 	Email: Luizfcneto123@gmail.com
+ * 	Descrição: Receber o salario bruto de um funcionario
+ *   e o valor da prestação. Eles receberao uma linha de 
+ *   credito e o valor maximo da prestação tem que 
+ *   ser 30% do valor bruto de seu salario 
+ * 	Entrada: Salario Bruto (double), 
+ *   valor da prestação (double)
+ * 	Saida: Emprestimo aceito ou nao (boolean)
+ * 
+ * 
+ */
+
+import java.util.Scanner;
+
+public class Exercicio15 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		double grossSalary, provision;
+		boolean allowed = false;
+		System.out.println("Informe o seu salario bruto e o valor da prestação, respectivamente: ");
+		grossSalary = scan.nextDouble();
+		provision = scan.nextDouble();
+		if (provision <= grossSalary*0.3 && provision > 0) {
+			allowed = true;
+		}
+		System.out.println(allowed);
+	}
+	
+}
