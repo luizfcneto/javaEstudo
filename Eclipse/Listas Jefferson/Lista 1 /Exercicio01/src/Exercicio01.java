27cfd394a48ce40934758fb51786b548a603cec1
Luiz <luizfcneto123@gmail.com>
2018-03-18 18:05:14 -0300

    +Acrescentada Pasta de Lista de Exercicios do Jefferson/ + lista 1 concluida

---
+++ Eclipse/Listas Jefferson/Lista 1 /Exercicio01/src/Exercicio01.java
@@ -0,0 +1,43 @@
+/*
+ * 		Exercicio01
+ * 	Autor: Luiz Fernando (Luizfcneto)
+ * 	Email: Luizfcneto123@gmail.com	
+ * 	Descrição: Determinar se um numero inteiro é primo;
+ * 	Entrada: numero (int) -> number;
+ * 	Saida: É primo ou nao (boolean) -> prime; 
+ * 
+ */
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main( String[] args ) {
+		Scanner scan = new Scanner( System.in );
+		int number, counter = 3;
+		boolean prime = true;
+		
+		System.out.println( "Informe o numero: " );
+		number = scan.nextInt();
+		
+		if( number == 1 || number == 2 ) 
+			prime = true;
+		
+		else if( number % 2 == 0 )
+			prime = false;
+		
+		else {
+			while ( counter != number ) {
+				if ( number % counter == 0 ) {
+					prime = false;
+					break;
+				} 
+				
+				counter += 2;
+			}			
+		}
+		
+		System.out.println("O numero: " + number + " é primo? " + prime );
+	}
+	
+}
---
+++ Eclipse/Listas Jefferson/Lista 1 /Exercicio03/src/Exercicio03.java
@@ -0,0 +1,30 @@
+/*
+ * 		Exercicio03
+ * 	Autor: Luiz Fernando (Luizfcneto)
+ * 	Email: Luizfcneto123@gmail.com
+ * 	Descrição: Contar a quantidade de numeros pares em uma lista de inteiros
+ * 	Entrada: n numeros (int) -> numbers
+ * 	Saida: quantidade de pares na lista (int) -> counterEven 
+ *
+ */
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+	
+	public static void main( String[] args ) {
+		Scanner scan = new Scanner( System.in );
+		int counterEven  = 0, numbers = 0;
+		
+		while ( numbers != -1 ) {
+			System.out.println("Informe o numero: [Para parar de inserir, digite -1]");
+			numbers = scan.nextInt();
+			
+			if( numbers % 2 == 0 )
+				counterEven++;
+		}
+		
+		System.out.println( "Quantidade de pares: " + counterEven );
+	}
+	
+}
